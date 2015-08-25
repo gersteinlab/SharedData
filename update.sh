5045201c7b94c7fa081d30dc76f09ed851744f0e
Shantao Li <shantao.li@yale.edu>
2015-08-25 15:27:12 -0400

    fixedupdate.sh

--- update.sh
+++ update.sh
@@ -10,6 +10,12 @@ done
 #MSG+="\""
 echo $MSG
 
-git add -A
-git commit -am $MSG
+{
+git add -A && 
+git commit -am $MSG &&
 git push
+} || {
+    echo "Code failed!"
+    echo "Not updated!"
+    exit
+}
